a1f367038fce01e22b77dc70613f1b6239828092
Ram2305 <vadapalli.j@northeastern.edu>
2024-02-21 04:51:36 -0500

    packer valiadte workflow added

--- test.js
+++ test.js
@@ -1,40 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const test = express();
+const app = express();
 
-test.use(bodyParser.json());
+app.use(bodyParser.json());
 
-let userData = {
-    id: '1',
-    username: 'testuser',
-    first_name: 'Test',
-    last_name: 'User'
-};
+let userData = {};
+
+app.post('/v1/user', (req, res) => {
+    const { id, username, first_name, last_name } = req.body;
+    if (!id || !username || !first_name || !last_name) {
+        return res.status(400).send('Missing required user data');
+    }
+    userData = { id, username, first_name, last_name };
+    res.status(201).send(userData);
+});
 
 const basicAuthMiddleware = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     if (!authHeader || !authHeader.startsWith('Basic ')) {
         return res.status(401).send('Authentication required');
     }
-
     const base64Credentials = authHeader.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [username, password] = credentials.split(':');
 
-    if (username !== 'testuser' || password !== 'testpassword') {
+    if (username !== userData.username || password !== 'testpassword') {
         return res.status(403).send('Access Denied');
     }
-
     next();
 };
 
-test.put('/v1/user/self', basicAuthMiddleware, (req, res) => {
+app.put('/v1/user/self', basicAuthMiddleware, (req, res) => {
     userData = { ...userData, ...req.body };
     res.status(200).send(userData);
 });
 
-test.get('/v1/user/self', basicAuthMiddleware, (req, res) => {
+app.get('/v1/user/self', basicAuthMiddleware, (req, res) => {
     res.status(200).send(userData);
 });
 
-module.exports = test;
+module.exports = app;
